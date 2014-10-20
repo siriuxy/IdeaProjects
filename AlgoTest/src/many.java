b0b10126950da1694d9fa20da2698efc56deb447
siriuxy <likaiy5@gmail.com>
2014-10-19 23:41:36 -0500

    first commit

---
+++ AlgoTest/src/many.java
@@ -0,0 +1,11 @@
+
+import java.util.Arrays;
+
+
+public class many {
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        int[] a=new int[5];
+    }
+}
---
+++ testOnMain/src/com/company/many.java
@@ -0,0 +1,11 @@
+//import company.*;
+/**
+ * Created by siriux on 10/11/14.
+ */
+public class many {
+    public static void main(String[] args) {
+        // write your code hexre
+        System.out.println("hey");
+    }
+
+}
